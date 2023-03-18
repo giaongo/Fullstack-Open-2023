658fe710e7f8d7bdf9c847669e2ea1c5f649e29a
Giao Ngo <giaongo.97@gmail.com>
2023-03-18 20:44:37 +0200

    add toggle functionality. Complete 5.5 and 5.6

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,14 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import BlogsDisplay from './components/BlogsDisplay';
 import LoginForm from './components/LoginForm';
 import NewBlogForm from './components/NewBlogForm';
 import Notification from './components/Notification';
+import Toggleble from './components/Toggleble';
 const App = () => {
   const [user, setUser] = useState(null);
   const [token,setToken] = useState(null);
   const [update, setUpdate] = useState(false);
   const [notificationMessage, setNotificationMessage] = useState('');
   const [notificationStatus, setNotificationStatus] = useState('');
+  const blogFormRef = useRef();
   const logoutUser = () => {
     setUser('');
     setToken('');
@@ -31,15 +33,28 @@ const App = () => {
         <Notification status={notificationStatus} message={notificationMessage}/>
       ): null}
       {!user
-        ? <LoginForm setUser={setUser} setToken={setToken} setNotificationStatus = {setNotificationStatus} setNotificationMessage={setNotificationMessage}/>
+        ? <LoginForm
+          setUser={setUser}
+          setToken={setToken}
+          setNotificationStatus = {setNotificationStatus}
+          setNotificationMessage={setNotificationMessage}/>
         : (
           <>
             <span>{user.name} logged in</span>
             <button style={{ marginLeft:10 }} onClick={logoutUser}>Logout</button>
-            <NewBlogForm token = {token} updateFunction={{ update, setUpdate }}  setNotificationStatus = {setNotificationStatus} setNotificationMessage = {setNotificationMessage} />
+            <Toggleble buttonLabel = "create new blog" ref={blogFormRef}>
+              <NewBlogForm
+                token = {token}
+                updateFunction={{ update, setUpdate }}
+                setNotificationStatus = {setNotificationStatus}
+                setNotificationMessage = {setNotificationMessage}
+                blogFormRef={blogFormRef} />
+            </Toggleble>
+
             <BlogsDisplay token={token} update={update}/>
           </>
         ) }
+
     </div>
   );
 };
