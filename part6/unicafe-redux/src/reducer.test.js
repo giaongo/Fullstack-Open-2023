512dd62d70b228fd10eff73f29f1de0d907f899f
Giao Ngo <giaongo.97@gmail.com>
2023-07-27 16:14:07 +0300

    complete 6.1 and 6.2

--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -32,4 +32,54 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test("ok is incremented",() => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good:0,
+      ok:1, 
+      bad:0
+    })
+
+  })
+
+  test("bad is incremented", () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good:0,
+      ok:0,
+      bad:1
+    })
+  })
+
+  test("zero reset", () => {
+    const action = {
+      type: 'ZERO'
+    }    
+    const state = initialState
+    deepFreeze(state)
+    const newState = counterReducer(state,action)
+    expect(newState).toEqual(state)
+  })
+
+  test("others return initial state",() => {
+    const action = {
+      type: 'abd'
+    }
+    const state = initialState
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual(state)
+  })
+
 })
