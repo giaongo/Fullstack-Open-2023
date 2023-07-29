ac85cad76415f784878531f4201b535585dedb55
Giao Ngo <giaongo.97@gmail.com>
2023-07-29 13:55:29 +0300

    complete 6.6, 6.7, 6.8

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,43 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote, addNewAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  // function triggers action dispatching for incrementing vote
-  const vote = (id) => {
-    dispatch(voteAnecdote(id))
-  }
-
-  // function triggers action dispatching for adding new anecdote
-  const addNew = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ""
-    console.log("new anecdote is ",content);
-    dispatch(addNewAnecdote(content))
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={addNew}>
-        <div><input name="anecdote"/></div>
-        <button>create</button>
-      </form>
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   )
 }
---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { addNewAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch()
+    // function triggers action dispatching for adding new anecdote
+    const addNew = (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        event.target.anecdote.value = ""
+        console.log("new anecdote is ",content);
+        dispatch(addNewAnecdote(content))
+    }
+    
+    return (
+    <>
+        <h2>create new</h2>
+        <form onSubmit={addNew}>
+        <div><input name="anecdote"/></div>
+        <button>create</button>
+        </form>
+    </>
+    )
+}
+
+export default AnecdoteForm
